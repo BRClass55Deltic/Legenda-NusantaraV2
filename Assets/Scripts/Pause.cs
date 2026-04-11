9be70ed60a51aa2c6fe88a12a260476b1408a4aa
LMSCoronation <daffa.a.aryasatya@gmail.com>
2026-04-11 11:18:54 +0700

    Initial PauseLogic
    No logic yet

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;    
+
+public class Pause : MonoBehaviour
+{
+    
+   // public PausePannel pausePannel;
+
+    bool isPaused;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
