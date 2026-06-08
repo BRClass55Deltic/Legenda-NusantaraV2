e4f28c233883acfd66cd50ddab1bd75466ad5ad6
LMSCoronation <daffa.a.aryasatya@gmail.com>
2026-06-08 16:26:42 +0700

    Lv 3 Enemy Chasing + Anim_DONE

--- Assets/Scripts/EnemyChasing.cs
+++ Assets/Scripts/EnemyChasing.cs
@@ -9,12 +9,16 @@ public class EnemyChasing : MonoBehaviour
     public string playerTag = "Player";
     private NavMeshAgent agent;
     private Transform player;
+
+    private Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.speed = moveSpeed;
+
+        anim = GetComponent<Animator>();
 
         GameObject p = GameObject.FindGameObjectWithTag(playerTag);
         if (p != null)
@@ -26,17 +30,28 @@ public class EnemyChasing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player == null /*|| isCaught*/) return;
-         ChasePlayer();
+        if (player == null /*|| isCaught*/) 
+        {
+            // Jika tidak ada player, pastikan animasi chase mati
+            if (anim != null) anim.SetBool("isChasing", false);
+            return;
+        }
+        ChasePlayer();
     }
 
      void ChasePlayer()
     {
         agent.SetDestination(player.position);
 
+    
         if (agent.velocity.sqrMagnitude > 0.1f)
         {
+            if (anim != null) anim.SetBool("isChasing", true); 
             transform.rotation = Quaternion.LookRotation(agent.velocity);
+        }
+        else
+        {
+            if (anim != null) anim.SetBool("isChasing", false); 
         }
     }
 
